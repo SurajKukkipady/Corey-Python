12980ec3a33c8db62660d4b5b71fa01ed6b46f9f
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2026-06-10 22:39:55 +0530

    Create 3_int_and_float.py

---
+++ Python_beginner/3_int_and_float.py
@@ -0,0 +1,12 @@
+num = 1
+
+print(type(num))
+
+#Aritmatic operations
+# Addition 1+ 2
+# Subtraction 1-2
+# Multiplication 1*2
+# Division 1/2
+# Floor division 1//2
+# Modulo 1%2
+# Exponentiation 1**2
