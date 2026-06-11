3cc978ff9fc2322c04cbd87fbe4a308848732cfe
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2026-06-11 19:07:46 +0530

    Update 4_list_tup_set.py

--- Python_beginner/4_list_tup_set.py
+++ Python_beginner/4_list_tup_set.py
@@ -1,4 +1,41 @@
+# Lists, tuples and sets
+# Lists are mutable, tuples are immutable, sets are unordered and unindexed
+
+
 courses = ['History', 'Math', 'Physics', 'CompSci']
 print(courses)
 print(len(courses))
 print(courses[0])
+print(courses[3])
+print(courses[-1])
+print(courses[0:2])
+courses.append('Art')
+print(courses)
+courses.insert(0, 'Education')
+print(courses)
+
+courses_2 = ['Education', 'Art']
+print(courses_2)
+
+courses_3 = courses + courses_2
+print(courses_3)
+
+courses_3.remove('Education')
+print(courses_3)
+
+popped = courses_3.pop()
+print(courses_3)
+print(popped)
+
+courses_3.reverse()
+print(courses_3)
+courses_3.sort()
+print(courses_3)
+courses_3.sort(reverse=True)
+print(courses_3)
+
+print(courses_3.index('Education'))
+
+for index, course in enumerate(courses_3, start=1):
+    print(index, course)
+
