24c3de28e1c5ed90415ae1440e10a5cd1d6ab294
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2026-06-15 21:43:34 +0530

    Update 6_dict.py

--- Python_beginner/6_dict.py
+++ Python_beginner/6_dict.py
@@ -8,3 +8,15 @@ print(student["name"])
 print(student.get("name"))
 print(student.get("phone"))
 print(student.get("phone", "Not Found"))
+
+student.update({"name": "Johnny", "age": 29, 'phone': '555-5555'})
+print(student)
+
+print(len(student))
+print(student.keys())
+print(student.values())
+
+print(student.items())
+
+for key, value in student.items():
+    print(key, value)
