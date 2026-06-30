c0afbc7e9795875fa3b95ff604c0fbada6795385
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2026-06-30 20:42:59 +0530

    Create 7_Conditionals_and_Booleans.py

---
+++ Python_beginner/7_Conditionals_and_Booleans.py
@@ -0,0 +1,17 @@
+# Comparisons:
+# Equal: ==
+# Not equal: !=
+# Greater than: >
+# Less than: <
+# Greater than or equal to: >=
+# Less than or equal to: <=
+# Object identity: is
+
+language = 'Python'
+
+if language == 'Python':
+    print('Language is Python')
+elif language == 'Java':
+    print('Language is Java')
+else:
+    print('No match')
