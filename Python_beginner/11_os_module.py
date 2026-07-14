3508ec2fb1ffdd263ab82c5a099eed93897aef3a
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2026-07-14 21:00:42 +0530

    Update 11_os_module.py

--- Python_beginner/11_os_module.py
+++ Python_beginner/11_os_module.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 #print(dir(os))
 print(os.getcwd())
@@ -6,3 +7,10 @@ os.chdir("C:/Users/suraj/OneDrive/Documents/Corey_Python")
 print(os.getcwd())
 
 print(os.listdir())
+
+print(os.stat('Python_beginner'))
+mod_time = os.stat('Python_beginner').st_mtime
+print(mod_time)
+print(datetime.fromtimestamp(mod_time))
+
+os.walk()
