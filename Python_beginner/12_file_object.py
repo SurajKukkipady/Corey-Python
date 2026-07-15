953ec42491db717f741bf1255cf1024d9f74095d
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2026-07-15 20:29:59 +0530

    file

---
+++ Python_beginner/12_file_object.py
@@ -0,0 +1,21 @@
+# File Objects
+
+f = open('text.txt', 'r')
+print(f.name)
+f.close()
+
+print('######## Using Context Managers #########')
+
+with open('text.txt', 'r') as f:
+    f_contents = f.read()
+    print(f_contents)
+    f.seek(0) # Because the curor will be in the end of the file
+    f_contents = f.readlines()
+    print(f_contents)
+    f.seek(0)
+    print('######### Using for loop ########')
+    for line in f:
+        print(line, end='')
+
+        
+
